a0a26664902ef9b6898cbbb24a968fe3cee1891e
Luca Terzaroli <luca.terzaroli@opicino.it>
2020-04-21 09:59:53 +0200

    modules version updated

--- backend/__init__.py
+++ backend/__init__.py
@@ -39,29 +39,5 @@ def create_app(pyenv=None):
     with app.app_context():
         from .dgt_api.v1_0 import digitest_api as rest_api
         api.add_resource(rest_api.CallUrlRes, '/api/v1.0/request_url/<method>')
-        #
-        # api.add_resource(rest_api.NodeListRes, '/nodesList')
-        # api.add_resource(rest_api.NodeListApi, '/api/v1.0/nodes_list')
-        #
-        # api.add_resource(rest_api.VisitorsRes, '/nodes/<node_id>')
-        # api.add_resource(rest_api.VisitorsApi, '/api/v1.0/visitors/<node_id>')
-        #
-        # api.add_resource(rest_api.AgeOverviewRes, '/ages/<node_id>')
-        # api.add_resource(rest_api.AgeOverviewApi, '/api/v1.0/ages/<node_id>')
-        #
-        # api.add_resource(rest_api.MeanVisitorRes, '/mean_visitors/<node_id>')
-        # api.add_resource(rest_api.MeanVisitorApi, '/api/v1.0/mean_visitors/<node_id>')
-        #
-        # api.add_resource(rest_api.GenderVisitorsRes, '/gender/<node_id>')
-        # api.add_resource(rest_api.GenderVisitorsApi, '/api/v1.0/visitors_gender/<node_id>')
-        #
-        # api.add_resource(rest_api.ContactsPerHourRes, '/punctual_visitors/<node_id>')
-        # api.add_resource(rest_api.ContactsPerHourApi, '/api/v1.0/punctual_visitors/<node_id>')
-        #
-        # api.add_resource(rest_api.VisitPerGenderAndAgeRes, '/visit-per-gender-and-age/<node_id>')
-        # api.add_resource(rest_api.VisitPerGenderAndAgeApi, '/api/v1.0/visit-per-gender-and-age/<node_id>')
-        #
-        # api.add_resource(rest_api.UpdateDbRes, '/updateDb/<node_id>')
-        # api.add_resource(rest_api.UpdateDbApi, '/api/v1.0/updateDb')
         app.register_blueprint(__api_bp)
         return app
